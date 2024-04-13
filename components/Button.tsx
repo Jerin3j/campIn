db835e129b4bd12c58bf409de8fb9c430550f33c
Jerin jj <jerinjj202@gmail.com>
2024-04-13 15:40:53 +0530

    Get App Section added

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,14 +6,15 @@ type ButtonProps = {
     type: 'button' | 'submit',
     title: string,
     icon?: string, 
-    varient: string
+    varient: string,
+    full?: boolean
 }
 
-const Button = ({type, title, icon, varient}: ButtonProps) => {
+const Button = ({type, title, icon, varient, full}: ButtonProps) => {
   return (
     <button
     type={type}
-    className={`flexCenter gap-3 rounded-full border ${varient}`}
+    className={`flexCenter gap-3 rounded-full border ${varient} ${full && 'w-full'}`}
     >
     { icon && <Image src={icon} alt={title} width={24} height={24}/>}
     <label className={'font-bold whitespace-nowrap'}>{title}</label>
--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import Button from './Button'
+import Image from 'next/image'
 
 const GetApp = () => {
   return (
-    <div>GetApp</div>
+    <section className='flexCenter w-full flex-col pb-[100px]'>
+      <div className="get-app">
+        <div className="z-20 flex flex-1 flex-col w-full gap-12
+          items-start justify-center">
+          <h2 className="bold-40 lg:bold-64xl:max-w-[320px]"> Get for free now</h2>
+          <p className="regular-16 text-gray-10">
+            Available on IOS and Android
+          </p>
+          <div className="Buttons flex flex-col w-full gap-3 whitespace-nowrap 
+            xl:flex-row xl:max-w-[450px]">
+            <Button
+            type='button'
+            title='Download App'
+            icon='/apple.svg'
+            varient='btn_white'
+            full
+            />
+            <Button
+            type='button'
+            title='Download App'
+            icon='/android.svg'
+            varient='btn_dark_green_outline'
+            full
+            />
+          </div>
+        </div>
+        <div className="flexEnd flex-1">
+          <Image
+           src={'/phones.png'}
+           alt='phones'
+           width={550}
+           height={870}
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
